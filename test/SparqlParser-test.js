f399745f3e0399ee2c8404df4829b0a080b4c24b
Ruben Verborgh <ruben@verborgh.org>
2014-04-22 17:27:18 +0200

    Report a syntax error if the query is invalid.

--- test/SparqlParser-test.js
+++ test/SparqlParser-test.js
@@ -17,4 +17,15 @@ describe('A SparqlParser instance', function () {
       expect(sparqlParser.parse(query)).to.deep.equal(parsedQuery);
     });
   });
+
+  it('should throw a SparqlSyntaxError on an invalid query', function () {
+    var query = 'invalid', error = null;
+    try { sparqlParser.parse(query); }
+    catch (e) { error = e; }
+
+    expect(error).to.exist;
+    expect(error).to.be.an.instanceof(SparqlParser.SparqlSyntaxError);
+    expect(error).to.have.property('message', 'Syntax error in SPARQL query: Expected [2] Query or [30] Update but "i" found.');
+    expect(error).to.have.property('query', query);
+  });
 });
